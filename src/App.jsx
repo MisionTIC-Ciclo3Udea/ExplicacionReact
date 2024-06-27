f8f83718bdfcf8d2c4df3462e783a0d16613d2db
YAN <yancarlostano@gmail.com>
2024-06-26 22:43:04 -0500

    crear card razas de perros

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import './styles/App.css';
+import logo from './media/navbar.png';
+import borderCollie from './media/bordercollie.jpg';
+import rhodesian from './media/rhodesian.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <ul className='navbar'>
+            <li>
+                <img src={logo} alt='navbar perros' className='logo'/>
+            </li>
+            <li>
+                <button className="button mainButton">Nuevo Post</button>
+            </li>
+            <li>
+                <div className='buscar'>
+                    <input placeholder='Buscar una raza'/>
+                    <i className='fa fa-search button iconoBusqueda' aria-hidden='true'></i>
+                </div>
+            </li>
+            <li>
+                <button className='button secondaryButton'>Login</button>
+            </li>
+            <li>
+                <button className='button mainButton'>Registro</button>
+            </li>
+        </ul>
+    </header>
+    <main>
+        <section>
+            <h1>Razas de Perros</h1>
+            <ul className='breedCardContainer'>
+                <CardRazasPerros nombreRaza='BorderCollie' imagen={borderCollie}/>
+                <CardRazasPerros nombreRaza='Rhodesian' imagen={rhodesian}/>
+            </ul>
+        </section>
+        <section></section>
+    </main>
+    <footer></footer>
+    </div>
+  );
+}
+
+function CardRazasPerros({ nombreRaza, imagen} ) {
+    return (
+        <li className='breedCard'>
+            <div className='contenedorImagen'>
+                <img src={imagen} alt={nombreRaza}/>
+            </div>
+            <span className='breedTitle'>{nombreRaza}</span>
+        </li>
+    );
+}
+
+export default App;
