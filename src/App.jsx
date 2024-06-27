d1108c42914357cf6cc59ee94f72c93fa3b8aa1a
YAN <yancarlostano@gmail.com>
2024-06-26 23:14:51 -0500

    agregar jsconfig.json para tener imports absolutos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import './styles/App.css';
-import logo from './media/navbar.png';
-import borderCollie from './media/bordercollie.jpg';
-import rhodesian from './media/rhodesian.jpg';
+import CardRazasPerros from 'components/CardRazasPerros';
+import 'styles/App.css';
+import logo from 'media/navbar.png';
+import borderCollie from 'media/bordercollie.jpg';
+import rhodesian from 'media/rhodesian.jpg';
 
 function App() {
   return (
@@ -41,17 +42,6 @@ function App() {
     <footer></footer>
     </div>
   );
-}
-
-function CardRazasPerros({ nombreRaza, imagen} ) {
-    return (
-        <li className='breedCard'>
-            <div className='contenedorImagen'>
-                <img src={imagen} alt={nombreRaza}/>
-            </div>
-            <span className='breedTitle'>{nombreRaza}</span>
-        </li>
-    );
 }
 
 export default App;
