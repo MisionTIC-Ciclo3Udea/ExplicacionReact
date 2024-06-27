4159af0bae374a4b97e8db813e5036b1577ecafd
YAN <yancarlostano@gmail.com>
2024-06-27 00:07:58 -0500

    agregar cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,20 @@
-import CardRazasPerros from 'components/CardRazasPerros';
+import Index from 'pages';
+import RhodesianInfoPage from 'pages/rhodesian';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import 'styles/App.css';
-import logo from 'media/navbar.png';
-import borderCollie from 'media/bordercollie.jpg';
-import rhodesian from 'media/rhodesian.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <ul className='navbar'>
-            <li>
-                <img src={logo} alt='navbar perros' className='logo'/>
-            </li>
-            <li>
-                <button className="button mainButton">Nuevo Post</button>
-            </li>
-            <li>
-                <div className='buscar'>
-                    <input placeholder='Buscar una raza'/>
-                    <i className='fa fa-search button iconoBusqueda' aria-hidden='true'></i>
-                </div>
-            </li>
-            <li>
-                <button className='button secondaryButton'>Login</button>
-            </li>
-            <li>
-                <button className='button mainButton'>Registro</button>
-            </li>
-        </ul>
-    </header>
-    <main>
-        <section>
-            <h1>Razas de Perros</h1>
-            <ul className='breedCardContainer'>
-                <CardRazasPerros nombreRaza='BorderCollie' imagen={borderCollie}/>
-                <CardRazasPerros nombreRaza='Rhodesian' imagen={rhodesian}/>
-            </ul>
-        </section>
-        <section></section>
-    </main>
-    <footer></footer>
+        <Router>
+            <Routes>
+                <Route path='/rhodesian' element={<RhodesianInfoPage />} />
+                <Route path='/' element={<Index />} />
+            </Routes>
+            
+
+        </Router>
+      
     </div>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,46 @@
 import CardRazasPerros from "components/CardRazasPerros";
+import logo from 'media/navbar.png';
+import borderCollie from 'media/bordercollie.jpg';
+import rhodesian from 'media/rhodesian.jpg';
 
 function Index() {
-    return <div>Hola soy el index</div>
+    return (
+        <div>
+            <header>
+        <ul className='navbar'>
+            <li>
+                <img src={logo} alt='navbar perros' className='logo'/>
+            </li>
+            <li>
+                <button className="button mainButton">Nuevo Post</button>
+            </li>
+            <li>
+                <div className='buscar'>
+                    <input placeholder='Buscar una raza'/>
+                    <i className='fa fa-search button iconoBusqueda' aria-hidden='true'></i>
+                </div>
+            </li>
+            <li>
+                <button className='button secondaryButton'>Login</button>
+            </li>
+            <li>
+                <button className='button mainButton'>Registro</button>
+            </li>
+        </ul>
+      </header>
+      <main>
+        <section>
+            <h1>Razas de Perros</h1>
+            <ul className='breedCardContainer'>
+                <CardRazasPerros nombreRaza='BorderCollie' imagen={borderCollie}/>
+                <CardRazasPerros nombreRaza='Rhodesian' imagen={rhodesian}/>
+            </ul>
+        </section>
+        <section></section>
+      </main>
+      <footer></footer>
+        </div>
+    )
 }
 
 export default Index;
